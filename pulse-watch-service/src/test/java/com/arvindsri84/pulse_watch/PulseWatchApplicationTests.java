8c1f65980806f206a19d177f7de5b0d43415c3a8
arvindsri84 <arvinddotsrivastava@gmail.com>
2025-08-17 20:52:48 +0530

    Project setup for Pulse Monitor

---
+++ pulse-watch-service/src/test/java/com/arvindsri84/pulse_watch/PulseWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arvindsri84.pulse_watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
